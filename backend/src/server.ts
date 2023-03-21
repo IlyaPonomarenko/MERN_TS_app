0813c61fbfea85376905c198ec716c1c66c83f0a
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-03-21 17:05:12 +0200

    Add, Get, Get one

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
-import express from 'express'
+import app from "./app"
+import env from "./utils/validateEnv"
+import mongoose from 'mongoose'
 
-const app = express()
-const port = 8000
+const port = env.PORT
 
-//Endpoint for a http request
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
-
-app.listen(port, () => console.log(`Server started at ${port}`))
+//Connecting to db
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log('Mongoose connected')
+    app.listen(port, () => console.log(`Server started at ${port}`))
+  })
+  .catch(console.error)
