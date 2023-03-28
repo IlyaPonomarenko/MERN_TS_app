c017cb89debad8f02fadb1a4cb4a399302bacb83
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-03-28 19:26:52 +0300

    NavBar init

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,14 +1,24 @@
+import { Container, Navbar } from "react-bootstrap";
 import { User } from "../models/user";
 
 interface NavBarProps{
     loggedInUser: User | null,
+    onSignUpClicked: () => void,
+    onLogInClicked: () => void,
+    onLogOutClicked: () => void
     
 }
 
-const NavBar = () => {
+const NavBar = ({loggedInUser, onSignUpClicked, onLogInClicked, onLogOutClicked}:NavBarProps) => {
     return (
         <div>
-            
+            <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
+                <Container>
+                    <Navbar.Brand>
+                        Notes app
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
         </div>
     );
 };
